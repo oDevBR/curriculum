b27c437773b2782d2a88bc19af8a3dbb36bdf2f1
Joao Aquino <33878228+whisoer@users.noreply.github.com>
2022-03-19 20:48:07 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Curriculum</title>
+      </Head>
+      <h1>Hello World Next!</h1>
+    </>
+  );
+}
