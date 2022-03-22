1ec5482cdcfd731f1b2bf709339f1d48a6857754
Joao Aquino <33878228+whisoer@users.noreply.github.com>
2022-03-21 23:52:10 -0300

    Add axe to web accessibility

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import { AppProps } from 'next/app'
+import React from 'react'
 
 import '../styles/global.scss'
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  import('react-dom').then((ReactDOM) => {
+    import('@axe-core/react').then((axe) => {
+      axe.default(React, ReactDOM, 1000, {})
+    })
+  })
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
