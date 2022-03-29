a93016e116e77318ac1d6a1c77e1bf63f2c765aa
Joao Aquino <33878228+whisoer@users.noreply.github.com>
2022-03-28 22:41:49 -0300

    Add donate button

---
+++ src/components/DonateButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export function DonateButton() {
+  return (
+    <button type="button" className={styles.donateButton}>
+      Buy me a coffee
+    </button>
+  )
+}
